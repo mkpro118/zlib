7e91c1cab3708cc2e1e2c6c327383dcc3cee2259
M K <mkpro118@gmail.com>
2024-07-09 17:13:16 -0500

    zlib BitReader implemented and tested

--- src/zlib.rs
+++ src/zlib.rs
@@ -1,1 +1,95 @@
+// Inspired from: https://pyokagan.name/blog/2019-10-18-zlibinflate/
 
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct BitReader<'a> {
+    mem: &'a [u8],
+    pos: usize,
+    byte: u8,
+    numbits: usize,
+}
+
+impl<'a> BitReader<'a> {
+    pub fn new(mem: &'a [u8]) -> Self {
+        Self {
+            mem,
+            pos: 0,
+            byte: 0,
+            numbits: 0,
+        }
+    }
+
+    fn read_byte(&mut self) -> u8 {
+        self.numbits = 0;
+        let b = self.mem[self.pos];
+        self.pos += 1;
+        b
+    }
+
+    fn read_bit(&mut self) -> u8 {
+        if self.numbits <= 0 {
+            self.byte = self.read_byte();
+            self.numbits = 8;
+        }
+
+        self.numbits -= 1;
+
+        // shift bit out of byte
+        let bit = self.byte & 1;
+        self.byte >>= 1;
+
+        bit
+    }
+
+    fn read_bits(&mut self, n: usize) -> usize {
+        let mut out = 0usize;
+
+        for i in 0..n {
+            out |= (self.read_bit() as usize) << i;
+        }
+
+        out
+    }
+
+    fn read_bytes(&mut self, n: usize) -> usize {
+        // read bytes as an integer in little-endian
+        let mut out = 0usize;
+
+        for i in 0..n {
+            out |= (self.read_byte() as usize) << (8 * i);
+        }
+
+        out
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_bit() {
+        let mut reader = BitReader::new(b"\x9d");
+        assert_eq!(reader.read_bit(), 1);
+        assert_eq!(reader.read_bit(), 0);
+        assert_eq!(reader.read_bit(), 1);
+        assert_eq!(reader.read_bit(), 1);
+        assert_eq!(reader.read_bit(), 1);
+        assert_eq!(reader.read_bit(), 0);
+        assert_eq!(reader.read_bit(), 0);
+        assert_eq!(reader.read_bit(), 1);
+    }
+
+    #[test]
+    fn test_read_bits() {
+        let mut reader = BitReader::new(b"\x2b\x01");
+        assert_eq!(reader.read_bits(9), 299);
+    }
+
+    #[test]
+    fn test_read_byte() {
+        let mut reader = BitReader::new(b"\x66\x36");
+        assert_eq!(reader.read_bytes(2), 13926);
+    }
+}
