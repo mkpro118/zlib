769035e1a87cea5b8246e4aeee0db29291a921d2
M K <mkpro118@gmail.com>
2024-07-11 16:24:07 -0500

    Move constants to huffman.rs

--- src/zlib/decompress.rs
+++ src/zlib/decompress.rs
@@ -2,26 +2,7 @@
 //! Inspired from: [this article](https://pyokagan.name/blog/2019-10-18-zlibinflate/)
 
 use crate::zlib::bitreader::BitReader;
-use crate::zlib::huffman::{
-    distance_tree_alphabet, literal_length_tree_alphabet, HuffmanTree,
-};
-
-static LENGTH_EXTRA_BITS: [usize; 29] = [
-    0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5,
-    5, 5, 5, 0,
-];
-static LENGTH_BASE: [usize; 29] = [
-    3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 23, 27, 31, 35, 43, 51, 59,
-    67, 83, 99, 115, 131, 163, 195, 227, 258,
-];
-static DISTANCE_EXTRA_BITS: [usize; 30] = [
-    0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10,
-    11, 11, 12, 12, 13, 13,
-];
-static DISTANCE_BASE: [usize; 30] = [
-    1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97, 129, 193, 257, 385, 513,
-    769, 1025, 1537, 2049, 3073, 4097, 6145, 8193, 12289, 16385, 24577,
-];
+use crate::zlib::huffman::*;
 
 /// Decompresses DEFLATE-compressed data.
 ///
