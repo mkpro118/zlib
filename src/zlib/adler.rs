324a89ca32b2d6514546c09a8382018a8535cb08
M K <mkpro118@gmail.com>
2024-07-13 14:30:55 -0500

    Simplify adler, return u32 instead

--- src/zlib/adler.rs
+++ src/zlib/adler.rs
@@ -1,16 +1,11 @@
-const ADLER_MODULO: u16 = 65521;
+const ADLER_MODULO: u32 = 65521;
 
-pub struct Adler {
-    pub a: u16,
-    pub b: u16,
-}
+pub fn adler32(data: &[u8]) -> u32 {
+    let (a, b) = data.iter().fold((1u32, 0u32), |(mut a, mut b), &byte| {
+        a = (a + (byte as u32)) % ADLER_MODULO;
+        b = (b + a) % ADLER_MODULO;
+        (a, b)
+    });
 
-impl Adler {
-    pub fn from(data: &[u8]) -> Self {
-        data.iter().fold(Self { a: 1, b: 0 }, |mut adler, &byte| {
-            adler.a = (adler.a + (byte as u16)) % ADLER_MODULO;
-            adler.b = (adler.b + adler.a) % ADLER_MODULO;
-            adler
-        })
-    }
+    b << 16 | a
 }
