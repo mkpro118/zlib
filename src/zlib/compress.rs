8a4f294ce662796a9dc23b26ea705f51e6e843a2
M K <mkpro118@gmail.com>
2024-07-14 16:31:29 -0500

    Implemented and tested fixed compression

--- src/zlib/compress.rs
+++ src/zlib/compress.rs
@@ -3,8 +3,9 @@
 use crate::zlib::adler::adler32;
 use crate::zlib::bitwriter::BitWriter;
 use crate::zlib::huffman::{
-    get_distance_code, get_length_code, HuffmanTree, ZLIB_MAX_STRING_LENGTH,
-    ZLIB_MIN_STRING_LENGTH, ZLIB_WINDOW_SIZE,
+    get_distance_code, get_length_code, HuffmanTree, DISTANCE_BASE,
+    DISTANCE_EXTRA_BITS, LENGTH_BASE, LENGTH_EXTRA_BITS,
+    ZLIB_MAX_STRING_LENGTH, ZLIB_MIN_STRING_LENGTH, ZLIB_WINDOW_SIZE,
 };
 use crate::zlib::lz77::{LZ77Compressor, LZ77Unit};
 use LZ77Unit::{Literal, Marker};
@@ -62,8 +63,12 @@ fn compress_raw(writer: &mut BitWriter, data: &[u8]) {
     let n_blocks = data.len().div_ceil(SIXTEEN_KB) - 1;
 
     for (curr_block, chunk) in data.chunks(SIXTEEN_KB).enumerate() {
+        // BFINAL
         writer.write_bit(u8::from(curr_block == n_blocks));
+
+        // BTYPE
         writer.write_bits(0b00, 2);
+
         // Write length of block
         let len = data.len() as u16;
         let bytes = [(len & 0xff) as u8, (len >> 8) as u8];
@@ -80,6 +85,11 @@ fn compress_raw(writer: &mut BitWriter, data: &[u8]) {
 }
 
 fn compress_fixed(writer: &mut BitWriter, data: &[u8]) {
+    // BFINAL = 1, we only write one massive block
+    writer.write_bit(0b1);
+    // BTYPE
+    writer.write_bits(0b01, 2);
+
     let compressor = get_zlib_compressor();
     let (mut length_tree, mut distance_tree) = HuffmanTree::get_zlib_fixed();
     length_tree.assign();
@@ -114,16 +124,22 @@ fn literal_writer(ltree: &HuffmanTree, writer: &mut BitWriter, byte: char) {
 }
 
 fn length_writer(ltree: &HuffmanTree, writer: &mut BitWriter, length: usize) {
-    assert!(length > 3, "Length too short!");
-    assert!(length <= 258, "Length too long!");
+    assert!(length >= 3, "Length too short found {}!", length);
+    assert!(length <= 258, "Length too long found {}!", length);
 
-    let length = get_length_code(length) as u32;
-    let length =
-        char::from_u32(length).expect("Should convert, already checked");
-    let Some((code, len)) = ltree.encode(length) else {
-        panic!("No encoding found for '{}'", length);
-    };
-    writer.write_bits(code, len);
+    let code = get_length_code(length);
+    let symbol = char::from_u32(code as u32).unwrap();
+    let (huffman_code, huffman_len) = ltree.encode(symbol).unwrap();
+
+    // Write Huffman code for the length symbol
+    writer.write_bits(huffman_code, huffman_len);
+
+    // Write extra bits if needed
+    let extra_bits = LENGTH_EXTRA_BITS[code - 257];
+    if extra_bits > 0 {
+        let extra_value = length - LENGTH_BASE[code - 257];
+        writer.write_bits(extra_value, extra_bits);
+    }
 }
 
 fn distance_writer(
@@ -131,16 +147,25 @@ fn distance_writer(
     writer: &mut BitWriter,
     distance: usize,
 ) {
-    assert!(distance > 1, "Distance too short!");
-    assert!(distance < 32768, "Distance too long!");
+    assert!(distance >= 1, "Distance too short!");
+    assert!(distance <= 32768, "Distance too long!");
 
-    let distance = get_distance_code(distance) as u32;
-    let distance =
-        char::from_u32(distance).expect("Should convert, already checked");
-    let Some((code, len)) = dtree.encode(distance) else {
-        panic!("No encoding found for '{}'", distance);
-    };
-    writer.write_bits(code, len);
+    let code = get_distance_code(distance);
+    let symbol =
+        char::from_u32(code as u32).expect("Should convert, already checked");
+    let (huffman_code, huffman_len) = dtree
+        .encode(symbol)
+        .expect(format!("No encoding for {code}").as_str());
+
+    // Write 5-bit distance code
+    writer.write_bits(huffman_code, huffman_len);
+
+    // Write extra bits if needed
+    let extra_bits = DISTANCE_EXTRA_BITS[code];
+    if extra_bits > 0 {
+        let extra_value = distance - DISTANCE_BASE[code];
+        writer.write_bits(extra_value, extra_bits);
+    }
 }
 
 #[allow(dead_code)]
@@ -149,4 +174,65 @@ fn get_zlib_compressor() -> LZ77Compressor {
     compressor.min_match_length = ZLIB_MIN_STRING_LENGTH;
     compressor.max_match_length = ZLIB_MAX_STRING_LENGTH;
     compressor
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::zlib::decompress;
+    use std::{
+        fs,
+        path::{Path, PathBuf},
+    };
+
+    fn walkdir(top: &Path) -> Vec<PathBuf> {
+        assert!(top.is_dir(), "Top is not a directory (top = {top:?})");
+        top.read_dir()
+            .expect("Should read the dir")
+            .filter(|e| e.is_ok())
+            .map(|e| e.unwrap())
+            .map(|e| e.path())
+            .filter(|path| {
+                path.file_stem().is_some_and(|stem| {
+                    !stem.to_str().is_some_and(|x| x.starts_with("."))
+                })
+            })
+            .fold(vec![], |mut paths, entry| {
+                if entry.is_file() {
+                    paths.push(entry);
+                } else {
+                    paths.extend_from_slice(&walkdir(&entry));
+                }
+                paths
+            })
+    }
+
+    #[test]
+    fn test_fixed_on_license() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let license = root.join("LICENSE");
+        let bytes = fs::read(license).expect("Read file!");
+
+        let compressed = compress(&bytes, &Strategy::Fixed);
+        let decompressed =
+            decompress(&compressed).expect("Correct decompression");
+
+        assert_eq!(bytes, decompressed);
+    }
+
+    #[test]
+    fn test_fixed_on_source_files() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let src = root.join("src");
+
+        for file in walkdir(&src) {
+            let bytes = fs::read(file).expect("Read file!");
+
+            let compressed = compress(&bytes, &Strategy::Fixed);
+            let decompressed =
+                decompress(&compressed).expect("Correct decompression");
+
+            assert_eq!(bytes, decompressed);
+        }
+    }
 }
