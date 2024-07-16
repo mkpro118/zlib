e6eaf42b95534de352fcf6aad8bb3b77ae623f9e
M K <mkpro118@gmail.com>
2024-07-15 20:04:43 -0500

    Refactor compress/decompress test to integration tests

---
+++ tests/test_zlib.rs
@@ -0,0 +1,88 @@
+#[cfg(test)]
+mod tests {
+    use mini_git::zlib::{compress, compress::Strategy, decompress};
+    use std::{
+        fs,
+        path::{Path, PathBuf},
+    };
+
+    fn walkdir(top: &Path) -> Vec<PathBuf> {
+        assert!(top.is_dir(), "Top is not a directory (top = {top:?})");
+        top.read_dir()
+            .expect("Should read the dir")
+            .filter(|e| e.is_ok())
+            .map(|e| e.unwrap())
+            .map(|e| e.path())
+            .filter(|path| {
+                path.file_stem().is_some_and(|stem| {
+                    !stem.to_str().is_some_and(|x| x.starts_with("."))
+                })
+            })
+            .fold(vec![], |mut paths, entry| {
+                if entry.is_file() {
+                    paths.push(entry);
+                } else {
+                    paths.extend_from_slice(&walkdir(&entry));
+                }
+                paths
+            })
+    }
+
+    #[test]
+    fn test_fixed_on_license() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let license = root.join("LICENSE");
+        let bytes = fs::read(license).expect("Read file!");
+
+        let compressed = compress(&bytes, &Strategy::Fixed);
+        let decompressed =
+            decompress(&compressed).expect("Correct decompression");
+
+        assert_eq!(bytes, decompressed);
+    }
+
+    #[test]
+    fn test_fixed_on_source_files() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let src = root.join("src");
+
+        for file in walkdir(&src) {
+            let bytes = fs::read(file).expect("Read file!");
+
+            let compressed = compress(&bytes, &Strategy::Fixed);
+            let decompressed =
+                decompress(&compressed).expect("Correct decompression");
+
+            assert_eq!(bytes, decompressed);
+        }
+    }
+
+    #[test]
+    fn test_dynamic_on_license() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let license = root.join("LICENSE");
+        let bytes = fs::read(license).expect("Read file!");
+
+        let compressed = compress(&bytes, &Strategy::Dynamic);
+        let decompressed =
+            decompress(&compressed).expect("Correct decompression");
+
+        assert_eq!(bytes, decompressed);
+    }
+
+    #[test]
+    fn test_dynamic_on_source_files() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let src = root.join("src");
+
+        for file in walkdir(&src) {
+            let bytes = fs::read(file).expect("Read file!");
+
+            let compressed = compress(&bytes, &Strategy::Dynamic);
+            let decompressed =
+                decompress(&compressed).expect("Correct decompression");
+
+            assert_eq!(bytes, decompressed);
+        }
+    }
+}
