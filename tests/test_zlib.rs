8696015e850466a4b9c4dcebc6ffd2914c6bd1bb
M K <mkpro118@gmail.com>
2024-07-17 17:45:59 -0500

    Test auto compression

--- tests/test_zlib.rs
+++ tests/test_zlib.rs
@@ -1,10 +1,10 @@
 #[cfg(test)]
 mod tests {
-    use zlib::{compress, compress::Strategy, decompress};
     use std::{
         fs,
         path::{Path, PathBuf},
     };
+    use zlib::{compress, compress::Strategy, decompress};
 
     fn walkdir(top: &Path) -> Vec<PathBuf> {
         assert!(top.is_dir(), "Top is not a directory (top = {top:?})");
@@ -78,6 +78,35 @@ mod tests {
             let bytes = fs::read(file).expect("Read file!");
 
             let compressed = compress(&bytes, &Strategy::Dynamic);
+            let decompressed =
+                decompress(&compressed).expect("Correct decompression");
+
+            assert_eq!(bytes, decompressed);
+        }
+    }
+
+    #[test]
+    fn test_auto_on_license() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let license = root.join("LICENSE");
+        let bytes = fs::read(license).expect("Read file!");
+
+        let compressed = compress(&bytes, &Strategy::Auto);
+        let decompressed =
+            decompress(&compressed).expect("Correct decompression");
+
+        assert_eq!(bytes, decompressed);
+    }
+
+    #[test]
+    fn test_auto_on_source_files() {
+        let root = Path::new(env!("CARGO_MANIFEST_DIR"));
+        let src = root.join("src");
+
+        for file in walkdir(&src) {
+            let bytes = fs::read(file).expect("Read file!");
+
+            let compressed = compress(&bytes, &Strategy::Auto);
             let decompressed =
                 decompress(&compressed).expect("Correct decompression");
 
